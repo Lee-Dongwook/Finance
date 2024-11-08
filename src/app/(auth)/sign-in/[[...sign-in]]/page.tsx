a604d6bba157b3f1cb133f518dcdf72eae955433
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-11-08 10:52:02 +0900

    feat : Sign-in / Sign-up Page

---
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,29 @@
+import { ClerkLoaded, ClerkLoading, SignIn } from "@clerk/nextjs";
+import { Loader2 } from "lucide-react";
+import Image from "next/image";
+
+export default function SignInPage() {
+  return (
+    <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
+      <div className="h-full lg:flex flex-col items-center justify-center px-4">
+        <div className="text-center space-y-4 pt-16">
+          <h1 className="font-bold text-3xl text-[#2E2A47]">반갑습니다!</h1>
+          <p className="text-base text-[#7E8CA0]">
+            대시보드에 접근하기 위하여 로그인 혹은 계정을 만들어주세요!
+          </p>
+        </div>
+        <div className="flex justify-center items-center mt-8">
+          <ClerkLoaded>
+            <SignIn path="/sign-in" />
+          </ClerkLoaded>
+          <ClerkLoading>
+            <Loader2 className="animate-spin text-muted-foreground" />
+          </ClerkLoading>
+        </div>
+      </div>
+      <div className="h-full bg-blue-600 hidden lg:flex items-center justify-center">
+        <Image src="" alt="Logo" height={100} width={100} />
+      </div>
+    </div>
+  );
+}
