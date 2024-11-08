16a613f4cf0f56ab812b9ec317f6304b11091ec8
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-11-08 10:11:21 +0900

    feat : drizzle-orm migrate

---
+++ src/scripts/migrate.ts
@@ -0,0 +1,20 @@
+import { neon } from "@neondatabase/serverless";
+import { config } from "dotenv";
+import { drizzle } from "drizzle-orm/neon-http";
+import { migrate } from "drizzle-orm/neon-http/migrator";
+
+config({ path: ".env.local" });
+
+export const sql = neon(process.env.NEXT_PUBLIC_DATABASE_URL as string);
+export const db = drizzle(sql);
+
+const main = async () => {
+  try {
+    await migrate(db, { migrationsFolder: "drizzle" });
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+main();
