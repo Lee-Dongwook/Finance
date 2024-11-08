e259f51e6c46bf0e24adf234d644b9165ac4e8a6
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-11-08 13:19:13 +0900

    feat : Transaction API 작성

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -2,12 +2,19 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
 import accounts from "./account";
+import categories from "./categories";
+import transactions from "./transactions";
+import summary from "./summary";
 
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 
-const routes = app.route("/accounts", accounts);
+const routes = app
+  .route("/accounts", accounts)
+  .route("/categories", categories)
+  .route("/transactions", transactions)
+  .route("/summary", summary);
 
 export const GET = handle(app);
 export const POST = handle(app);
