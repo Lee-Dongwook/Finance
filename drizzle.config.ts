720b415321e216ac16565efa2b6379fd99fb8832
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-11-08 09:24:21 +0900

    feat : drizzle-kit setting

---
+++ drizzle.config.ts
@@ -0,0 +1,15 @@
+import { config } from "dotenv";
+import { defineConfig } from "drizzle-kit";
+
+config({ path: ".env.local" });
+
+export default defineConfig({
+  out: "./drizzle",
+  schema: "./src/db/schema.ts",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.NEXT_PUBLIC_DATABASE_URL ?? "",
+  },
+  verbose: true,
+  strict: true,
+});
