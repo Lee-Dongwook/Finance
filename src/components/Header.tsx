d931a69128dee76d76238c69c959126e18f268c8
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-11-08 10:06:05 +0900

    feat : Header Component

---
+++ src/components/Header.tsx
@@ -0,0 +1,25 @@
+import HeaderLogo from "@/components/HeaderLogo";
+import { ClerkLoaded, ClerkLoading, UserButton } from "@clerk/nextjs";
+import { Loader2 } from "lucide-react";
+
+const Header = () => {
+  return (
+    <header className="bg-gradient-to-b from-blue-700 to-blue-500 px-4 py-8 lg:px-14 pb-36">
+      <div className="max-w-screen-2xl mx-auto">
+        <div className="w-full flex justify-between items-center mb-14">
+          <div className="flex items-center lg:gap-x-16">
+            <HeaderLogo />
+          </div>
+          <ClerkLoaded>
+            <UserButton afterSignOutUrl="/" />
+          </ClerkLoaded>
+          <ClerkLoading>
+            <Loader2 className="size-8 animate-spin text-slate-400" />
+          </ClerkLoading>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
